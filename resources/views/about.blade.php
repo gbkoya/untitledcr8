76877f53cfe292c430e76f67ee3508828a73d9a2
Ebuka Peter <peterdbrainy5@gmail.com>
2022-08-29 13:41:20 +0100

    Working on the navbar

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('description',
+    'Archware - Africa\'s Leading Software Development Company
+    Software development company helping businesses and individuals take advantage of the benefits of the global technology
+    landscape.')
+@section('title', 'Archware - Excellent Tech-based Solutions For Your Business')
+@section('article:section', 'Home')
+@section('url', 'www.archwareltd.com')
+@section('image'){{ asset('customImages/Logo.svg') }}@stop
+@section('content')
+
+
+
+
+    <script type="text/javascript">
+    </script>
+@endsection
