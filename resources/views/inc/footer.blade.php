ae35540b4320a65b3bb568bd2cb848f5c4a8b777
Ebuka Peter <peterdbrainy5@gmail.com>
2022-08-29 14:15:01 +0100

    start work on the footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,169 +1,10 @@
-<!-- <section>
-    <div class="">
-
-        <footer class="text-center text-white" style="background-color: #1f3f95;">
-
-            <div class="container p-5">
-                <div class="m-4">
-                    <h3 class="fw-bold" style="font-size:30px;">Newsletter</h3>
-                    <p class="pt-2" style="font-size:medium;">
-                        Be the first to know about discounts, offers and events weekly <br> in your mailbox. Unsubscribe
-                        whenever you like one click
-                    </p>
-                </div>
-
-                <section class="m-3">
-                    <form>
-                        <div class="row d-flex justify-content-center">
-
-                            <div class="TopHeader" style=" width:70%">
-                                <div class="InputBox">
-                                    <input type="search" placeholder="Enter your email" id="SearchInput" style="background-color: #1f3f95; color:white;">
-                                    <div class="col-auto">
-                                        <button type="submit" class="btn btn-outline-light mb-4 footer_submit_button" style="font-weight: 700;font-size: 13px;line-height: 16px;align-items: center;text-align: center;">
-                                            Submit
-                                        </button>
-                                    </div>
-                                    <ul class="SearchResult" id="SearchResult"></ul>
-                                </div>
-                            </div>
-
-                        </div>
-
-                    </form>
-                </section>
-            </div>
-
-            <hr>
-
-            <section class="container mt-5 p-4">
-
-                <div class="row footer-css">
-
-                    <div class="col-md-6 col-sm-3" style="text-align: initial;">
-                        <h5 class="pb-3">
-                            <a href="#" id="LoginDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src="{{ asset('customImages/Footer logo.svg') }}" alt="archwareLog default profile pix" class="user__photo" style="height: 5rem; width: 5.8rem; margin-right: 0.2rem">
-                            </a>
-                        </h5>
-
-                        <ul class="list-unstyled mb-0">
-                            <li class="pb-3"style="font-size:medium;">
-                                 Archware Technologies is a result-driven tech company established<br> 
-                                 to provide technological innovations that help improve the quality<br> 
-                                 of work and lives of people across several business sectors.                    
-                            </li>
-                        </ul>
-                        <section class="pb-3">
-                            <a class="m-1 btn btn-outline-light" href="#!" role="button"><i class="fab fa-facebook"></i></a>
-
-                            <a class="m-1 btn btn-outline-light" href="#!" role="button"><i class="fab fa-youtube"></i></a>
-
-                            <a class="m-1 btn btn-outline-light" href="#!" role="button"><i class="fab fa-instagram"></i></a>
-
-                            <a class="m-1 btn btn-outline-light" href="#!" role="button"><i class="fab fa-whatsapp"></i></a>
-                        </section>
-                    </div>
-
-
-                    <div class="col-md-2 col-sm-3" style="text-align: initial; flex-direction:column">
-                        <h5 class="pb-3 fw-bold" style="font-size: 20px;">Pages</h5>
-
-                        <ul class="list-unstyled mb-0 fw-lighter">
-                            <li class="pb-2">
-                                <a href="#!" class="text-white text-decoration-none" style="font-size:medium;">Home</a>
-                            </li>
-                            <li class="pb-2">
-                                <a href="#!" class="text-white text-decoration-none" style="font-size:medium;">About Us</a>
-                            </li>
-                            <li class="pb-2">
-                                <a href="#!" class="text-white text-decoration-none" style="font-size:medium;">Services</a>
-                            </li>
-                            <li class="pb-2">
-                                <a href="#!" class="text-white text-decoration-none" style="font-size:medium;">Subsidiaries</a>
-                            </li>
-                            <li class="pb-2">
-                                <a href="#!" class="text-white text-decoration-none" style="font-size:medium;">Franchise</a>
-                            </li>
-                        </ul>
-                    </div>
-
-
-
-                    <div class="col-md-2 col-sm-3" style="text-align: initial">
-                        <h5 class="pb-3 fw-bold" style="font-size: 20px;">Subsidiaries</h5>
-
-                        <ul class="list-unstyled mb-0 fw-lighter">
-                            <li class="pb-2" style="font-size:medium;">Youhi Media</li>
-                            <li class="pb-2" style="font-size:medium;">Formost Eyeclinic</li>
-                            <li class="pb-2" style="font-size:medium;">Archware Foodbank</li>
-                            <li class="pb-2" style="font-size:medium;">Archware Consulting</li>
-                            <li class="pb-2"style="font-size:medium;">Farm Management</li>
-                        </ul>
-                    </div>
-
-
-                    <div class="col-md-2 col-sm-3" style="text-align: initial">
-                        <h5 class="pb-3 fw-bold" style="font-size: 20px;">Location</h5>
-
-                        <ul class="list-unstyled mb-0 fw-lighter">
-                            <li class="pb-2" style="font-size:medium;">
-                                3, Ayanboye Street,<br>Anthony Village, Lagos
-                            </li>
-                            <li class="pb-2" style="font-size:medium;">
-                                Ph: +234709154822
-                            </li>
-                        </ul>
-                    </div>
-
-                </div>
-
-            </section>
-
-            <div class="text-center pb-5 mt-5">
-                © Archware Technologies International Limited 2022. All Rights Reserved.
-            </div>
-
-        </footer>
-
-    </div>
-</section> -->
 
 <section>
     <div class="">
 
-        <footer class="text-center text-white" style="background-color: #1f3f95;">
+        <footer class="text-center text-white" style="background-color: #3F5675;">
 
-            <div class="container pt-5">
-                <div class="m-4">
-                    <h3 class="fw-bold newsletter-style" style="font-size:30px;">Newsletter</h3>
-                    <p class="pt-2 newsletter-paragraph text-center" style="font-size:medium;">
-                        Be the first to know about discounts, offers and events weekly <br> in your mailbox. Unsubscribe
-                        whenever you like one click
-                    </p>
-                </div>
-
-                <section class="m-3">
-                    <form>
-                        <div class="row d-flex justify-content-center">
-
-                            <div class="TopHeader" style=" width:70%">
-                                <div class="InputBox">
-                                    <input type="search" placeholder="Enter your email" id="SearchInput" style="background-color: #1f3f95; color:white;">
-                                    <div class="col-auto">
-                                        <button type="submit" class="btn btn-outline-light mb-4 footer_submit_button" style="font-weight: 700;font-size: 13px;line-height: 16px;align-items: center;text-align: center;">
-                                            Submit
-                                        </button>
-                                    </div>
-                                    <ul class="SearchResult" id="SearchResult"></ul>
-                                </div>
-                            </div>
-
-                        </div>
-
-                    </form>
-                </section>
-            </div>
+            
 
             <hr>
 
@@ -174,15 +15,15 @@
                     <div class="" style="text-align: initial;">
                         <h5 class="pb-3">
                             <a href="#" id="LoginDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src="{{ asset('customImages/Footer logo.svg') }}" alt="archwareLog default profile pix" class="user__photo" style="height: 5rem; width: 5.8rem; margin-right: 0.2rem">
+                                <img src="{{ asset('customImages/logo-whitevariation.png') }}" alt="archwareLog default profile pix" class="user__photo" style="margin-right: 0.2rem">
                             </a>
                         </h5>
 
                         <ul class="list-unstyled mb-0">
                             <li class="pb-3 footer-aboutus"style="font-size:medium;">
-                                 Archware Technologies is a result-driven tech company established<br> 
-                                 to provide technological innovations that help improve the quality<br> 
-                                 of work and lives of people across several business sectors.                    
+                                 Foremost Eye Clinic is dedicated to acquiring innovative eye<br> 
+                                 health services to meet the growing demands of local,<br> 
+                                 cross-border, and global-markets.                    
                             </li>
                         </ul>
                         <section class="pb-3">
@@ -198,44 +39,42 @@
 
 
                     <div class="pages" style="text-align: initial; flex-direction:column">
-                        <h5 class="pb-3 fw-bold" style="font-size: 20px;">Pages</h5>
+                        <h5 class="pb-3 fw-bold" style="font-size: 20px;">About Us</h5>
 
                         <ul class="list-unstyled mb-0 fw-lighter">
                             <li class="pb-2">
-                                <a href="#!" class="text-white text-decoration-none" style="font-size:medium;">Home</a>
+                                <a href="#!" class="text-white text-decoration-none" style="font-size:medium;">Vision</a>
                             </li>
                             <li class="pb-2">
-                                <a href="#!" class="text-white text-decoration-none" style="font-size:medium;">About Us</a>
+                                <a href="#!" class="text-white text-decoration-none" style="font-size:medium;">Mission</a>
                             </li>
                             <li class="pb-2">
-                                <a href="#!" class="text-white text-decoration-none" style="font-size:medium;">Services</a>
+                                <a href="#!" class="text-white text-decoration-none" style="font-size:medium;">Facilities</a>
                             </li>
                             <li class="pb-2">
-                                <a href="#!" class="text-white text-decoration-none" style="font-size:medium;">Subsidiaries</a>
+                                <a href="#!" class="text-white text-decoration-none" style="font-size:medium;">Workforce</a>
                             </li>
-                            <li class="pb-2">
-                                <a href="#!" class="text-white text-decoration-none" style="font-size:medium;">Franchise</a>
-                            </li>
+                         
                         </ul>
                     </div>
 
 
 
                     <div class="pages" style="text-align: initial">
-                        <h5 class="pb-3 fw-bold" style="font-size: 20px;">Subsidiaries</h5>
+                        <h5 class="pb-3 fw-bold" style="font-size: 20px;">Services</h5>
 
                         <ul class="list-unstyled mb-0 fw-lighter">
-                            <li class="pb-2" style="font-size:medium;">Youhi Media</li>
-                            <li class="pb-2" style="font-size:medium;">Formost Eyeclinic</li>
-                            <li class="pb-2" style="font-size:medium;">Archware Foodbank</li>
+                            <li class="pb-2" style="font-size:medium;">Cataract & Glaucoma</li>
+                            <li class="pb-2" style="font-size:medium;">Retina</li>
+                            <li class="pb-2" style="font-size:medium;">Dry Eye Treatment</li>
                             <li class="pb-2" style="font-size:medium;">Archware Consulting</li>
-                            <li class="pb-2"style="font-size:medium;">Farm Management</li>
+                            <li class="pb-2"style="font-size:medium;">Contact Lenses</li>
                         </ul>
                     </div>
 
 
                     <div class="col-md-2 col-sm-3 location" style="text-align: initial">
-                        <h5 class="pb-3 fw-bold" style="font-size: 20px;">Location</h5>
+                        <h5 class="pb-3 fw-bold" style="font-size: 20px;">Contact Us</h5>
 
                         <ul class="list-unstyled mb-0 fw-lighter">
                             <li class="pb-2" style="font-size:medium;">
@@ -250,6 +89,37 @@
                 </div>
 
             </section>
+
+            <div class="container pt-5">
+                <div class="m-4">
+                    <h3 class="fw-bold newsletter-style" style="font-size:30px;">Subscribe to Our blog</h3>
+                    <p class="pt-2 newsletter-paragraph text-center" style="font-size:medium;">
+                       Know more about your eye health with our weekly curated facts<br> and resources.
+                       Unsubscribe whenever you want with one click.
+                    </p>
+                </div>
+
+                <section class="m-3">
+                    <form>
+                        <div class="row d-flex justify-content-center">
+
+                            <div class="TopHeader" style=" width:70%">
+                                <div class="InputBox">
+                                    <input type="search" placeholder="Enter your email" id="SearchInput" style="background-color: #1f3f95; color:white;">
+                                    <div class="col-auto">
+                                        <button type="submit" class="btn btn-outline-light mb-4 footer_submit_button" style="font-weight: 700;font-size: 13px;line-height: 16px;align-items: center;text-align: center;">
+                                            Submit
+                                        </button>
+                                    </div>
+                                    <ul class="SearchResult" id="SearchResult"></ul>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </form>
+                </section>
+            </div>
 
             <div class="text-center pb-5 mt-5">
                 © Archware Technologies International Limited 2022. All Rights Reserved.
