dfd4bbe9751464ab5f0888a4b0121155b6589b79
Ebuka Peter <peterdbrainy5@gmail.com>
2022-08-30 16:15:00 +0100

    Implement the Proof of work section

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -54,10 +54,63 @@
      </section>
     {{-- END OF VISION AND MISSION STATEMENT  --}}
 
+    <!-- PROOF OF WORK -->
+    <section class="container-fluid  proof-of-work">
+        <div class="d-sm-flex flex-wrap proof-of-work-child justify-content-around">
+            <div class="web">
+                <h1 class="incrementTimer" akhi="5">0</h1>
+                <p>Intercontinental <br>Clinics</p>
+            </div>
+            <div class="web">
+                <h1 class="incrementTimer" akhi="50">0</h1>
+                <p>Experienced<br> Specialists</p>
+            </div>
+            <div class="customers">
+                <h1 class="incrementTimer" akhi="100">0</h1>
+                <p>Happy <br>customers</p>
+            </div>
+            <div class="customers">
+                <h1 class="incrementTimer" akhi="24">0</h1>
+                <p>Hours <br>Open</p>
+            </div>
+        </div>
+    </section>
+    <!-- END OF PROOF OF WORK -->
+    {{-- PROOF OF WORK SECTION --}}
+
+
+    {{-- END OF PROOF OF WORK SECTION --}}
 
     </main>
     <body>
 
     <script type="text/javascript">
+     const counters = document.querySelectorAll('.incrementTimer');
+        const speed = 100;
+
+        const triggerCounter = (event) =>{
+            if (window.scrollY >= 400){
+                counters.forEach(counter => {
+            const animate = () => {
+                const value = +counter.getAttribute('akhi');
+                const data = +counter.innerText;
+
+                const time = value / speed;
+                if (data < value) {
+                    counter.innerText = Math.ceil(data + time);
+                    setTimeout(animate, 200);
+                } else {
+                    counter.innerText = value + '+';
+                }
+            }
+            animate();
+        });
+
+            }else{
+                return;
+            }
+        } 
+   
+        window.addEventListener('scroll', triggerCounter);
     </script>
 @endsection
