b126ea4ffbd528f165dbd4ef973f176652f7793f
Ebuka Peter <peterdbrainy5@gmail.com>
2022-09-02 10:39:04 +0100

    unknown change

---
+++ resources/views/blog/first_blog.blade.php
@@ -0,0 +1,109 @@
+@extends('layouts.app')
+
+@section('description',
+    'Archware - Africa\'s Leading Software Development Company
+    Software development company helping businesses and individuals take advantage of the benefits of the global technology
+    landscape.')
+@section('title', 'Archware - Excellent Tech-based Solutions For Your Business')
+@section('article:section', 'Home')
+@section('url', 'www.archwareltd.com')
+@section('image'){{ asset('customImages/Logo.svg') }}@stop
+
+@section('content')
+    <div class="mt-5">
+        {{-- Start --}}
+        <div class="FirstDiv">
+            <h1 class="FirstH1">The Effects Of<br> Screen Use On My<br>Child</h1>
+            <button class="FirstButton">READ MORE</button>
+        </div>
+        <!-- End of First Div -->
+    
+        <!-- Second div -->
+        <div class="everything">
+            <div class="next2">
+            <img src="/customImages/Frame 75.png" class="eye">
+            <div class="eyetext">
+                <p>How Much Do You Know About Pink Eye?</p>
+                <p class="secondtext">August 2022</p>
+                <p class="thirdtext">Have you ever had pink eye? Conjunctivitis is the inflammation of the conjunctiva, the outer layer of tissue covering the surface of the eyes and the inside of the eyelids....</p>
+                <button class="read">READ MORE</button>
+            </div>
+            </div>
+            <!-- 1 -->
+            <div class="next2" style="margin-top: 65.97px;">
+                <img src="/customImages/Frame 75.png" class="eye">
+                <div class="eyetext">
+                    <p>How Much Do You Know About Pink Eye?</p>
+                    <p class="secondtext">August 2022</p>
+                    <p class="thirdtext">Have you ever had pink eye? Conjunctivitis is the inflammation of the conjunctiva, the outer layer of tissue covering the surface of the eyes and the inside of the eyelids....</p>
+                    <button class="read">READ MORE</button>
+                </div>
+                </div> 
+                <!-- 2 -->
+                <div class="next2" style="margin-top: 65.97px;">
+                    <img src="/customImages/Frame 75.png" class="eye">
+                    <div class="eyetext">
+                        <p>How Much Do You Know About Pink Eye?</p>
+                        <p class="secondtext">August 2022</p>
+                        <p class="thirdtext">Have you ever had pink eye? Conjunctivitis is the inflammation of the conjunctiva, the outer layer of tissue covering the surface of the eyes and the inside of the eyelids....</p>
+                        <button class="read">READ MORE</button>
+                    </div>
+                    </div>
+                    <!-- 3 -->
+    
+                    <div class="next2" style="margin-top: 65.97px;">
+                        <img src="/customImages/Frame 75.png" class="eye">
+                        <div class="eyetext">
+                            <p>How Much Do You Know About Pink Eye?</p>
+                            <p class="secondtext">August 2022</p>
+                            <p class="thirdtext">Have you ever had pink eye? Conjunctivitis is the inflammation of the conjunctiva, the outer layer of tissue covering the surface of the eyes and the inside of the eyelids....</p>
+                            <button class="read">READ MORE</button>
+                        </div>
+                        </div>
+                        <!-- 5 -->
+    
+                        <div class="next2" style="margin-top: 65.97px;">
+                            <img src="/customImages/Frame 75.png" class="eye">
+                            <div class="eyetext">
+                                <p>How Much Do You Know About Pink Eye?</p>
+                                <p class="secondtext">August 2022</p>
+                                <p class="thirdtext">Have you ever had pink eye? Conjunctivitis is the inflammation of the conjunctiva, the outer layer of tissue covering the surface of the eyes and the inside of the eyelids....</p>
+                                <button class="read">READ MORE</button>
+                            </div>
+                            </div>
+                            <!-- 6 -->
+    
+                            <div class="next2" style="margin-top: 65.97px;">
+                                <img src="/customImages/Frame 75.png" class="eye">
+                                <div class="eyetext">
+                                    <p>How Much Do You Know About Pink Eye?</p>
+                                    <p class="secondtext">August 2022</p>
+                                    <p class="thirdtext">Have you ever had pink eye? Conjunctivitis is the inflammation of the conjunctiva, the outer layer of tissue covering the surface of the eyes and the inside of the eyelids....</p>
+                                    <button class="read">READ MORE</button>
+                                </div>
+                                </div>
+                                <!-- 7 -->
+    
+                                <div class="buttons">
+                                    <button class="activeNumberButton">1</button>
+                                    <button class="numberButton2">2</button>
+                                    <button class="numberButton3">3</button>
+                                    <button class="numberButton4">4</button>
+                                    <button class="numberButton5">5</button>
+                                    <button class="numberButton6">6</button>
+                                    <button class="numberButton7">7</button>
+                                    <button class="numberButton8">8</button>
+                                    <button class="numberButton9">9</button> 
+                                    <button class="numberButton0" style="margin-left: 49.29px;">50</button>
+                                    <button class="nextButton">Next</button>   
+                                    <p class="dotts">...</p>                            
+                                </div>
+    
+            </div>
+        <!-- End of Second Div -->
+    </div>
+
+
+    <script type="text/javascript"></script>
+
+@endsection
