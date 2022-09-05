5f9bd6161f97d544a1d45c27fccf9e0f73f267fa
Ebuka Peter <peterdbrainy5@gmail.com>
2022-09-05 17:22:11 +0100

    Implement the shop men hero nav and added the hero main copy

--- resources/views/shops/shop_men.blade.php
+++ resources/views/shops/shop_men.blade.php
@@ -10,14 +10,41 @@
 @section('content')
 
 <body>
-<main>
+{{-- <main> --}}
 {{-- HERO SECTION --}}
 <section class="container-fluid shop-hero">
+{{-- Sub nav --}}
+<nav class="d-flex flex-row flex-wrap container-fluid justify-content-evenly">
+{{-- <div class="d-flex flex-row"> --}}
+<div class="row d-flex search-wrapper ">
+    <input type="search"
+    placeholder="Search for eyewear, lenses and frames"
+    />
+    <img 
+    class="img-fluid search-shop-image"
+    src="{{ asset('customImages/arrow-right.png') }}"/>
+        </div>   
+        <div class="d-flex flex-row flex-wrap button-wrapper">
+            <a href=""><button class="login" type="button">Log In</button></a>
+            <a href=""><button class="signup" type="button">Sign Up</button></a>
+           <a href=""><button class="try-it" type="button">Try it On</button></a>
+        </div>
+            <button type="button" class="shop-button">
+                <img
+                src="{{ asset('customImages/buyIcon.png') }}"
+                />
+                CART
+            </button>
+{{-- </div>  --}}
+</nav>
 
+<h1>The Largest Online Store
+    for  Glasses and Contact
+    Lenses.</h1>
 </section>
 
 {{-- END OF HERO SECTION --}}
-</main>
+{{-- </main> --}}
 
 <script type="text/javascript">
     </script>
