f02bb399fec13d669a9b9e9e51db8ba5ddca5057
["JOY", "MirrorVlogger"] <fajendagbajoy@gmail.com>
2022-09-05 17:22:27 +0100

    service update

--- resources/views/services/first_service.blade.php
+++ resources/views/services/first_service.blade.php
@@ -22,43 +22,91 @@
 
     <section class="container-fluid mt-2 pt-2">
         <div class="d-flex flex-row flex-wrap justify-content-center">
-            <div class="card col-lg-3 mx-3 remove-shadow-mobile box-shadow">
-                <img src="/customImages/screening.png" class="card-img-top" alt="...">
-                <div class="archware-card-padding-left d-flex align-items-center flex-wrap p-4">
-                    <div class="pt-1 foremost-card-title">
-                        Visual Screening
-                    </div>
-                    <p class="pt-3 foremost-card-body">
-                        We offer vision screening services that are reliable and accurate, using the latest technology.
-                        With our visual screening services, you can be sure that you're getting the best care for your
-                        eyes.
-                    </p>
-                </div>
-            </div>
-            <div class="card col-lg-3 mx-3 remove-shadow-mobile box-shadow">
-                <img src="/customImages/eye exam.png" class="card-img-top" alt="...">
-                <div class="archware-card-padding-left d-flex align-items-center flex-wrap p-4">
+            <div class="mt-4 card col-lg-3 mx-3 remove-shadow-mobile box-shadow">
+                <img src="/customImages/Comprehensive Eye Examination.png" class="card-img-top" alt="...">
+                <div class="archware-card-padding-left d-flex align-items-center flex-wrap p-2">
                     <div class="pt-1 foremost-card-title">
                         Comprehensive Eye Examination
                     </div>
                     <p class="pt-3 foremost-card-body">
-                        A comprehensive eye examination is a perfect way to ensure that your vision is at its best. Our
-                        team of experts will evaluate everything from your prescription to the health of your eyes.
+                        Measure your visual acuity by undergoing the appropriate eye examination to determine the strength
+                        of your sight at a distance. A comprehensive eye examination is a perfect way to ensure that your
+                        vision is at its best. Our team of experts will evaluate everything from your prescription to the
+                        health of your eyes.
                     </p>
                 </div>
             </div>
-            <div class="card col-lg-3 mx-3 remove-shadow-mobile box-shadow">
-                <img src="/customImages/cataract.png" class="card-img-top" alt="...">
-                <div class="archware-card-padding-left d-flex align-items-center flex-wrap p-4">
+            <div class="mt-4 card col-lg-3 mx-3 remove-shadow-mobile box-shadow">
+                <img src="/customImages/Comprehensive Eye Examination.png" class="card-img-top" alt="...">
+                <div class="archware-card-padding-left d-flex align-items-center flex-wrap p-2">
                     <div class="pt-1 foremost-card-title">
-                        Cataract Surgery
+                        Comprehensive Eye Examination
                     </div>
                     <p class="pt-3 foremost-card-body">
-                        We specialise in Cataract Surgery and our services are second to none. We use the latest
-                        technologies and our highly skilled Ophthalmologists will have you see clearly in no time.
+                        Measure your visual acuity by undergoing the appropriate eye examination to determine the strength
+                        of your sight at a distance. A comprehensive eye examination is a perfect way to ensure that your
+                        vision is at its best. Our team of experts will evaluate everything from your prescription to the
+                        health of your eyes.
                     </p>
                 </div>
             </div>
+            <div class="mt-4 card col-lg-3 mx-3 remove-shadow-mobile box-shadow">
+                <img src="/customImages/Comprehensive Eye Examination.png" class="card-img-top" alt="...">
+                <div class="archware-card-padding-left d-flex align-items-center flex-wrap p-2">
+                    <div class="pt-1 foremost-card-title">
+                        Comprehensive Eye Examination
+                    </div>
+                    <p class="pt-3 foremost-card-body">
+                        Measure your visual acuity by undergoing the appropriate eye examination to determine the strength
+                        of your sight at a distance. A comprehensive eye examination is a perfect way to ensure that your
+                        vision is at its best. Our team of experts will evaluate everything from your prescription to the
+                        health of your eyes.
+                    </p>
+                </div>
+            </div>
+            <div class="mt-4 card col-lg-3 mx-3 remove-shadow-mobile box-shadow">
+                <img src="/customImages/Comprehensive Eye Examination.png" class="card-img-top" alt="...">
+                <div class="archware-card-padding-left d-flex align-items-center flex-wrap p-2">
+                    <div class="pt-1 foremost-card-title">
+                        Comprehensive Eye Examination
+                    </div>
+                    <p class="pt-3 foremost-card-body">
+                        Measure your visual acuity by undergoing the appropriate eye examination to determine the strength
+                        of your sight at a distance. A comprehensive eye examination is a perfect way to ensure that your
+                        vision is at its best. Our team of experts will evaluate everything from your prescription to the
+                        health of your eyes.
+                    </p>
+                </div>
+            </div>
+            <div class="mt-4 card col-lg-3 mx-3 remove-shadow-mobile box-shadow">
+                <img src="/customImages/Comprehensive Eye Examination.png" class="card-img-top" alt="...">
+                <div class="archware-card-padding-left d-flex align-items-center flex-wrap p-2">
+                    <div class="pt-1 foremost-card-title">
+                        Comprehensive Eye Examination
+                    </div>
+                    <p class="pt-3 foremost-card-body">
+                        Measure your visual acuity by undergoing the appropriate eye examination to determine the strength
+                        of your sight at a distance. A comprehensive eye examination is a perfect way to ensure that your
+                        vision is at its best. Our team of experts will evaluate everything from your prescription to the
+                        health of your eyes.
+                    </p>
+                </div>
+            </div>
+            <div class="mt-4 card col-lg-3 mx-3 remove-shadow-mobile box-shadow">
+                <img src="/customImages/Comprehensive Eye Examination.png" class="card-img-top" alt="...">
+                <div class="archware-card-padding-left d-flex align-items-center flex-wrap p-2">
+                    <div class="pt-1 foremost-card-title">
+                        Comprehensive Eye Examination
+                    </div>
+                    <p class="pt-3 foremost-card-body">
+                        Measure your visual acuity by undergoing the appropriate eye examination to determine the strength
+                        of your sight at a distance. A comprehensive eye examination is a perfect way to ensure that your
+                        vision is at its best. Our team of experts will evaluate everything from your prescription to the
+                        health of your eyes.
+                    </p>
+                </div>
+            </div>
+
         </div>
     </section>
 
