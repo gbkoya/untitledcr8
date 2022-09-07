73e0bef22d2284aecfed7aae9c7e992d42e78a04
unknown <ochiaghakella15@gmail.com>
2022-09-07 10:51:48 +0100

    test

--- resources/views/checkout1/checkout1.blade.php
+++ resources/views/checkout1/checkout1.blade.php
@@ -13,7 +13,7 @@
 <div class="everything-cart">
     <div class="formost-cart-first-div">
         <input placeholder="Search for eyewear, lenses and frames" class="formost-cart-first-div-input" ><img src="/customImages/Vector-arrow.png" class="arrow-icon-cart">
-            <button class="formost-cart-first-div-input-button">Log In</button>
+            <button class="formost-cart-first-div-input-button">Log In Test</button>
             <button class="formost-cart-first-div-input-button2">Sign Up</button>
             <button class="formost-cart-first-div-input-button3">TRY IT ON</button>   
             <button class="formost-cart-first-div-input-button4"><img src="customImages/Vector.png" class="cart-icon-cart">  Cart</button>     
