c60f44a9e44f5a28e9ab4494657b788c4ea51b9f
Tuvwehre <tuvwehre2018@gmail.com>
2022-09-07 16:55:30 +0100

    logintest

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
 'Archware - Africa\'s Leading Software Development Company
 Software development company helping businesses and individuals take advantage of the benefits of the global technology
 landscape.')
-@section('title', 'Archware - Excellent Tech-based Solutions For Your Business')
+@section('title', 'Archware - Excell ent Tech-based Solutions For Your Business')
 @section('article:section', 'Home')
 @section('url', 'www.archwareltd.com')
 @section('image'){{ asset('customImages/Logo.svg') }}@stop
