146dcb78d6d3bca5ebe56cf74eb3c39c647763ff
Ebuka Peter <peterdbrainy5@gmail.com>
2022-09-22 23:16:47 +0100

    Fix the bug in pagination script, and complete the shop pagination

--- resources/views/shops/shop_men.blade.php
+++ resources/views/shops/shop_men.blade.php
@@ -571,7 +571,7 @@
 // PAGINATION SCRIPT HERE
 const paginationNumbers = document.getElementById("pagination-numbers");
 const paginatedList = document.getElementById("paginated-list");
-const listItems = paginatedList.querySelectorAll(".productsData");
+const listItems = paginatedList.querySelectorAll(".cardClick");
 const nextButton = document.getElementById("next-button");
 const prevButton = document.getElementById("prev-button");
 
