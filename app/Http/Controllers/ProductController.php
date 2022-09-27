5701b8f82dd930f676fceb7334ecaf630bc84e3e
["JOY", "MirrorVlogger"] <fajendagbajoy@gmail.com>
2022-09-27 11:36:27 +0100

    swagger doc update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -26,14 +26,14 @@ class ProductController extends Controller
         * operationId="getProductById",
         * tags={"Get product by id"},
         * summary="Show product detail",
-        * description="Get the detail of a particular product. Comes with product price and features. Product ID must be passed in the url",
+        * description="Get the detail of a particular product. Comes with product price and features. Product ID must be passed in the url. [For the admin]",
         *     @OA\RequestBody(
         *         @OA\JsonContent(),
         *         @OA\MediaType(
         *            mediaType="multipart/form-data",
         *            @OA\Schema(
         *               type="object",
-        *               @OA\Property(property="product_id", type="text"),
+        *               @OA\Property(property="product_id", type="number"),
         *            ),
         *        ),
         *    ),
@@ -82,7 +82,7 @@ class ProductController extends Controller
         * operationId="getProductByCategory",
         * tags={"Get product by category"},
         * summary="Get product by category",
-        * description="Gets all products under the requested category. Product ID must be passed in the url",
+        * description="Gets all products under the requested category. Product ID must be passed in the url. [For the admin]",
         *     @OA\RequestBody(
         *         @OA\JsonContent(),
         *         @OA\MediaType(
@@ -138,7 +138,7 @@ class ProductController extends Controller
         * operationId="storeProduct",
         * tags={"Store product"},
         * summary="Add a new product to the system",
-        * description="Add a new product to the system. The product price, features and images are also added",
+        * description="Add a new product to the system. The product price, features and images are also added. [For the admin]",
         *     @OA\RequestBody(
         *         @OA\JsonContent(),
         *         @OA\MediaType(
@@ -149,7 +149,7 @@ class ProductController extends Controller
         *               @OA\Property(property="name", type="text"),
         *               @OA\Property(property="imagedirectory", type="file"),
         *               @OA\Property(property="features", type="text"),
-        *               @OA\Property(property="product_price", type="text"),
+        *               @OA\Property(property="product_price", type="number"),
         *            ),
         *        ),
         *    ),
@@ -236,7 +236,7 @@ class ProductController extends Controller
         * operationId="edit_product",
         * tags={"Edit product"},
         * summary="Edit existing product",
-        * description="Change or edit a product. Especially the product price. Product ID must be passed in the url",
+        * description="Change or edit a product. Especially the product price. Product ID must be passed in the url. [For the admin]",
         *     @OA\RequestBody(
         *         @OA\JsonContent(),
         *         @OA\MediaType(
@@ -247,7 +247,7 @@ class ProductController extends Controller
         *               @OA\Property(property="name", type="text"),
         *               @OA\Property(property="image", type="file"),
         *               @OA\Property(property="description", type="text"),
-        *               @OA\Property(property="price", type="text"),
+        *               @OA\Property(property="price", type="number"),
         *            ),
         *        ),
         *    ),
@@ -286,7 +286,7 @@ class ProductController extends Controller
         * operationId="delete_product",
         * tags={"Delete product"},
         * summary="Delete existing product",
-        * description="Remove a product from the system. Product ID must be passed in the url",
+        * description="Remove a product from the system. Product ID must be passed in the url. [For the admin]",
         *     @OA\RequestBody(
         *         @OA\JsonContent(),
         *         @OA\MediaType(
@@ -294,7 +294,7 @@ class ProductController extends Controller
         *            @OA\Schema(
         *               type="object",
         *               required={"product_id"},
-        *               @OA\Property(property="product_id", type="text"),
+        *               @OA\Property(property="product_id", type="number"),
         *            ),
         *        ),
         *    ),
